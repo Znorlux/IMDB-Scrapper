8da1aa98d0bf966919aac0b7cbd0d592bf1ba26e
Znorlux <mabe0898@hotmail.com>
2023-05-28 19:16:37 -0500

    Avance de grafo y metodos requeridos

--- Graph.py
+++ Graph.py
@@ -1,6 +1,7 @@
+from Scrapper import Movie
+import json
 import networkx as nx
-import matplotlib.pyplot as plt
-
+import os
 class Graph:
     def __init__(self, directed=True):
         self.adj_list = {}
@@ -12,9 +13,9 @@ class Graph:
         else:
             return []
 
-    def add_vertex(self, node_label):
+    def add_vertex(self, node_label, node_type=None):
         if node_label not in self.adj_list:
-            self.adj_list[node_label] = []
+            self.adj_list[node_label] = {'type': node_type, 'neighbors': []}
 
     def add_edge(self, v1, v2):
         if v1 not in self.adj_list:
@@ -22,9 +23,9 @@ class Graph:
         if v2 not in self.adj_list:
             self.add_vertex(v2)
 
-        self.adj_list[v1].append(v2)
+        self.adj_list[v1]['neighbors'].append(v2)
         if not self.directed:
-            self.adj_list[v2].append(v1)
+            self.adj_list[v2]['neighbors'].append(v1)
 
     def DFS(self, start, visited=None):
         if visited is None:
@@ -39,31 +40,113 @@ class Graph:
                 for n in neighbors:
                     self.DFS(n, visited)
         return visited
+
+    def find_most_frequent_actor(self):
+        actor_counts = {}
+        for node, attr in self.adj_list.items():
+            if attr['type'] == 'actor':
+                movies = set()  # Conjunto para almacenar las películas únicas en las que actúa el actor
+                for neighbor in attr['neighbors']:
+                    neighbor_attr = self.adj_list[neighbor]
+                    if neighbor_attr['type'] == 'movie':
+                        movies.add(neighbor)
+                movie_count = len(movies)
+                actor_counts[node] = movie_count
+
+        if not actor_counts:
+            return None, 0
+
+        most_frequent_actor = max(actor_counts, key=actor_counts.get)
+        max_movie_count = actor_counts[most_frequent_actor]
+
+        return most_frequent_actor, max_movie_count
+    
+    def find_most_frequent_director(self):
+        director_counts = {}
+        for node, attr in self.adj_list.items():
+            if attr['type'] == 'director':
+                movies = set()  # Conjunto para almacenar las películas únicas que dirigió el director
+                for neighbor in attr['neighbors']:
+                    neighbor_attr = self.adj_list[neighbor]
+                    if neighbor_attr['type'] == 'movie':
+                        movies.add(neighbor)
+                movie_count = len(movies)
+                director_counts[node] = movie_count
+
+        if not director_counts:
+            return None, 0
+
+        most_frequent_actor = max(director_counts, key=director_counts.get)
+        max_movie_count = director_counts[most_frequent_actor]
+
+        return most_frequent_actor, max_movie_count
     
 
+with open("movies.json", "r") as json_file:
+    data = json.load(json_file)
+
+# Crear una lista de películas con los datos del archivo JSON
+movie_list = []
+for movie_data in data:
+    title = movie_data["title"]
+    link = movie_data["link"]
+    actors = movie_data["actors"]
+    director = movie_data["director"]
+    writers = movie_data["writers"]
+    movie = Movie(title, link)
+    movie.actors = actors
+    movie.director = director
+    movie.writers = writers
+    movie_list.append(movie)
+
+# Crear el grafo y agregar nodos y aristas
 g = Graph()
-g.add_vertex("A")
-g.add_vertex("B")
-g.add_vertex("C")
-g.add_vertex("D")
-g.add_vertex("E")
-g.add_vertex("X")
-g.add_vertex("R")
 
+for movie in movie_list:
+    g.add_vertex(movie.title, node_type='movie')
+    for actor in movie.actors:
+        g.add_vertex(actor, node_type='actor')
+        g.add_edge(movie.title, actor)
+        g.add_edge(actor, movie.title)
+    if movie.director:
+        g.add_vertex(movie.director, node_type='director')
+        g.add_edge(movie.title, movie.director)
+        g.add_edge(movie.director, movie.title)
+    for writer in movie.writers:
+        g.add_vertex(writer, node_type='writer')
+        g.add_edge(movie.title, writer)
+        g.add_edge(writer, movie.title)
 
-g.add_edge("A", "B")
-g.add_edge("A", "E")
-g.add_edge("B", "C")
-g.add_edge("X", "R")
-g.add_edge("B", "D")
-g.add_edge("E", "X")
-g.DFS("A")
+most_frequent_actor, movie_count = g.find_most_frequent_actor()
+print("El actor que ha actuado en más películas es:", most_frequent_actor)
+print("Número de películas en las que actuó:", movie_count)
 
+print()
+
+most_frequent_director, director_movies = g.find_most_frequent_director()
+print("El director que ha dirigido en más películas es:", most_frequent_director)
+print("El director que ha dirigido en más películas es:", director_movies)
+
+print(g.get_neighbors("Charles Chaplin"))
+
+'''
+# Crear el grafo de Networkx
 G = nx.Graph()
-for node in g.adj_list:
-    G.add_node(node)
-    for neighbor in g.get_neighbors(node):
+for node, attr in g.adj_list.items():
+    G.add_node(node, **attr)
+    for neighbor in attr['neighbors']:
         G.add_edge(node, neighbor)
 
-nx.draw(G, with_labels=True, node_color='lightblue', edge_color='gray')
-plt.show()
+# Convertir los atributos a cadenas de texto
+for node in G.nodes:
+    attr = G.nodes[node]
+    for key, value in attr.items():
+        if isinstance(value, (list, type)):
+            attr[key] = str(value)
+
+if os.path.exists("graph.graphml"):
+    os.remove("graph.graphml")
+
+# Guardar el grafo en formato GraphML
+nx.write_graphml(G, "graph.graphml")
+'''
--- Scrapper.py
+++ Scrapper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-import mysql.connector
+import json 
+import os
 
 class Movie:
     def __init__(self, title, link):
@@ -10,85 +11,98 @@ class Movie:
         self.director = None
         self.writers = None
 
-url = 'https://www.imdb.com/chart/top/'
-response = requests.get(url)
-html = response.content
+if __name__ == "__main__":
+    url = 'https://www.imdb.com/chart/top/'
+    response = requests.get(url)
+    html = response.content
 
-soup = BeautifulSoup(html, 'html.parser')
-
-
-table = soup.find('tbody', class_='lister-list')
-rows = table.find_all('tr')
-movie_list = []
-
-for row in rows:
-    title_column = row.find('td', class_='titleColumn')
-    title_link = title_column.find('a')
-    title = title_link.get_text(strip=True)
-    href = title_link['href']
-    #print(f"Title: {title}")
-    #print(f"Href: www.imdb.com/{href}")
-    movie = Movie(title, "https://www.imdb.com"+href)
-    movie_list.append(movie)
+    soup = BeautifulSoup(html, 'html.parser')
 
 
-class ShowScrapper:
+    table = soup.find('tbody', class_='lister-list')
+    rows = table.find_all('tr')
+    movie_list = []
 
-    def Scrapper(self,movie):
+    for row in rows:
+        title_column = row.find('td', class_='titleColumn')
+        title_link = title_column.find('a')
+        title = title_link.get_text(strip=True)
+        href = title_link['href']
+        #print(f"Title: {title}")
+        #print(f"Href: www.imdb.com/{href}")
+        movie = Movie(title, "https://www.imdb.com"+href)
+        movie_list.append(movie)
 
-        link = movie.link
 
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    class ShowScrapper:
+
+        def Scrapper(self,movie):
+
+            link = movie.link
+
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+
+            response = requests.get(link, headers=headers)
+            html = response.content
+            soup = BeautifulSoup(html, 'html.parser')
+
+            cast_list = soup.find_all('a', attrs={'data-testid': 'title-cast-item__actor'})
+            actors = [actor.get_text(strip=True) for actor in cast_list]
+            movie.actors = actors
+            #print("ACTORES:")
+            #print(actors)
+            #for actor in actors:
+            #    print(actor)
+
+            #El primer elemento con ese nombre de clase, es el director, el resto tendrá a los escritores
+            director = soup.find('a', class_='ipc-metadata-list-item__list-content-item').get_text(strip=True)
+            movie.director = director
+            #print("\nDirector:")
+            #print(director)
+
+
+            writers_list = soup.find_all('a', class_='ipc-metadata-list-item__list-content-item')
+            writers = []
+            for writer in writers_list[1:-1]:
+                if writer.get_text(strip=True) in actors:
+                    break
+                else:
+                    writers.append(writer.get_text(strip=True))
+
+            movie.writers = writers
+            #print("\nEscritores:")
+            #print(writers)
+
+
+    #Realizar web scrapping a las 250 peliculas
+    ShowScrapper = ShowScrapper()
+    for i in range(len(movie_list)):
+        ShowScrapper.Scrapper(movie_list[i])
+        #A este punto tendre una lista con 250 objetos de tipo Movie los cuales tienen toda la informacion que necesito, asi que aqui ya debo
+        #mandar esos datos al JSON
+        print(movie_list[i].title)
+
+
+    if os.path.exists("movies.json"):
+        os.remove("movies.json")
+
+    data = []
+    for movie in movie_list:
+        movie_data = {
+            "title": movie.title,
+            "link": movie.link,
+            "actors": movie.actors,
+            "director": movie.director,
+            "writers": movie.writers
         }
+        data.append(movie_data)
 
-        response = requests.get(link, headers=headers)
-        html = response.content
-        soup = BeautifulSoup(html, 'html.parser')
-
-        cast_list = soup.find_all('a', attrs={'data-testid': 'title-cast-item__actor'})
-        actors = [actor.get_text(strip=True) for actor in cast_list]
-        movie.actors = actors
-        #print("ACTORES:")
-        #print(actors)
-        #for actor in actors:
-        #    print(actor)
-
-        #El primer elemento con ese nombre de clase, es el director, el resto tendrá a los escritores
-        director = soup.find('a', class_='ipc-metadata-list-item__list-content-item').get_text(strip=True)
-        movie.director = director
-        #print("\nDirector:")
-        #print(director)
+    # Escribir la lista de datos en un archivo JSON
+    with open("movies.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
 
 
-        writers_list = soup.find_all('a', class_='ipc-metadata-list-item__list-content-item')
-        writers = []
-        for writer in writers_list[1:-1]:
-            if writer.get_text(strip=True) in actors:
-                break
-            else:
-                writers.append(writer.get_text(strip=True))
-
-        movie.writers = writers
-        #print("\nEscritores:")
-        #print(writers)
-
-
-#Realizar web scrapping a las 250 peliculas
-ShowScrapper = ShowScrapper()
-#for i in range(len(movie_list)):
-#    ShowScrapper.Scrapper(movie_list[i])
-    #A este punto tendre una lista con 250 objetos de tipo Movie los cuales tienen toda la informacion que necesito, asi que aqui ya debo
-    #mandar esos datos a la base de datos
-    #print(movie_list[i].title)
-
-
-# Establecer la conexión a la base de datos
-conn = mysql.connector.connect(
-    host='localhost',
-    user='tu_usuario',
-    password='tu_contraseña',
-    database='nombre_de_la_base_de_datos'
-)
 
 
